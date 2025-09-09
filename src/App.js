94cd1586aeffa4d87522eeaf8ba49355948d435e
Dan Vogel <dhvogel2468@gmail.com>
2025-09-08 21:26:18 -0400

    landing pg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import SOMBookClubHome from './components/SOMBookClubHome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"     style={{
+    margin: 0,
+    padding: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: "url('/storm-photo.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}>
+      <SOMBookClubHome />
     </div>
   );
 }
---
+++ src/components/SOMBookClubHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { color, motion } from 'framer-motion'
+
+// SOMBookClubHome.jsx
+// Dark, moody homepage hero for a book club. Fullscreen background image.
+
+export default function SOMBookClubHome() {
+  return (
+
+        <p style={{color:"white", fontWeight:"bold", WebkitTextStroke: "0.2px black", fontSize: "100px"}}>SOM Book Club</p>
+  )
+}
