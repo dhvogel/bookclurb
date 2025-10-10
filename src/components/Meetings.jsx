6753a118dcc1350b73086f0e9f02c1a6452188b4
Dan Vogel <dhvogel2468@gmail.com>
2025-10-10 11:14:46 -0400

    add next meeting

--- src/components/Meetings.jsx
+++ src/components/Meetings.jsx
@@ -120,6 +120,11 @@ const Meetings = ({ user, db }) => {
   const meetings = [
     // Add more meetings here as needed
     {
+      id: "meeting-2025-10-16",
+      time: "Thu, 10/16, 6:00 PM EDT",
+      reading: "Empire of Pain, Ch 15-20",
+    },
+    {
       id: "meeting-2025-10-09",
       time: "Thu, 10/9, 6:00 PM EDT",
       reading: "Empire of Pain, Ch 11-14",
